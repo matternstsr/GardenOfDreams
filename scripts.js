15f006b297f06921895a16dd804aeb26a3df4ea8
LeeWest89 <lee.west@atlasschool.com>
2024-04-05 15:54:42 -0500

    Modified the Backgrounds UI for rotation between the 5 images selected.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const backgroundImages = [
+    'images/image1.jpeg',
+    'images/image2.jpeg',
+    'images/image3.jpeg',
+    'images/image4.jpeg',
+    'images/image5.jpeg'
+];
+
+let index = 0;
+
+function changeBackground() {
+    const background = document.getElementById('header');
+    const path = backgroundImages[index];
+    background.style.backgroundImage = `url(${path})`;
+    index = (index + 1) % backgroundImages.length;
+}
+
+changeBackground();
+setInterval(changeBackground, 5000);
