7bc2cdd5d63d96098d04a2de81f4111c8bcc217e
LeeWest89 <lee.west@atlasschool.com>
2024-04-08 19:01:40 -0500

    Added body element, About, dropdown, cards, and started makinging the carousel.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,50 @@
-const backgroundImages = [
-    'images/image1.jpeg',
-    'images/image2.jpeg',
-    'images/image3.jpeg',
-    'images/image4.jpeg',
-    'images/image5.jpeg'
-];
+$(document).ready(function() {
 
-let index = 0;
+// Background
 
-function changeBackground() {
-    const background = document.getElementById('header');
-    const path = backgroundImages[index];
-    background.style.backgroundImage = `url(${path})`;
-    index = (index + 1) % backgroundImages.length;
-}
+    const backgroundImages = [
+        'images/image1.jpeg',
+        'images/image2.jpeg',
+        'images/image3.jpeg',
+        'images/image4.jpeg',
+        'images/image5.jpeg'
+    ];
 
-changeBackground();
-setInterval(changeBackground, 10000);
+    let index = 0;
+
+    function changeBackground() {
+        const background = document.getElementById('backgroundPic');
+        const path = backgroundImages[index];
+        background.style.backgroundImage = `url(${path})`;
+        index = (index + 1) % backgroundImages.length;
+    }
+
+    changeBackground();
+    setInterval(changeBackground, 10000);
+
+    // States Carousel
+
+
+    $('.plantCarousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        centerMode: false,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ] 
+    });
+});
