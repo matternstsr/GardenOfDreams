9d42edbe9673fcf562d8e0d7d09d57a9f89dbda6
LeeWest89 <lee.west@atlasschool.com>
2024-04-10 17:03:04 -0500

    Pulling dropdown from json

--- scripts.js
+++ scripts.js
@@ -2,49 +2,90 @@ $(document).ready(function() {
 
 // Background
 
-    const backgroundImages = [
-        'images/image1.jpeg',
-        'images/image2.jpeg',
-        'images/image3.jpeg',
-        'images/image4.jpeg',
-        'images/image5.jpeg'
-    ];
+  const backgroundImages = [
+      'images/image1.jpeg',
+      'images/image2.jpeg',
+      'images/image3.jpeg',
+      'images/image4.jpeg',
+      'images/image5.jpeg'
+  ];
 
-    let index = 0;
+  let index = 0;
 
-    function changeBackground() {
-        const background = document.getElementById('backgroundPic');
-        const path = backgroundImages[index];
-        background.style.backgroundImage = `url(${path})`;
-        index = (index + 1) % backgroundImages.length;
-    }
+  function changeBackground() {
+      const background = document.getElementById('backgroundPic');
+      const path = backgroundImages[index];
+      background.style.backgroundImage = `url(${path})`;
+      index = (index + 1) % backgroundImages.length;
+  }
 
-    changeBackground();
-    setInterval(changeBackground, 10000);
+  changeBackground();
+  setInterval(changeBackground, 10000);
 
-    // States Carousel
+//DropDown
 
+  // const ddMenuDiv = document.getElementById("ddMenu");
 
-    $('.plantCarousel').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
-        centerMode: false,
-        infinite: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ] 
-    });
+  // fetch("statesData.json")
+  //   .then(response => response.json())
+  //   .then(data => {
+
+  //   if (data && data.length > 0) {
+  //     ddMenuDiv.innerHTML = '';
+
+  //     data.forEach(stateObject => {
+  //       const stateName = stateObject.state;
+  //       const dropdownItem = `<a class="dropdown-item text-center text-white" href="#">${stateName}</a>`;
+  //       ddMenuDiv.innerHTML += dropdownItem;
+  //     });
+  //   } else {
+  //     console.error("Error: Empty data or error fetching data");
+  //   }
+  // })
+  // .catch(error => console.error('Error fetching data:', error));
+  
+  const ddMenuDiv = document.getElementById("ddMenu");
+  
+  fetch("onestate.json")
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.hasOwnProperty("states")) {
+        ddMenuDiv.innerHTML = '';
+  
+        const statesObject = data.states;
+  
+        for (const stateName in statesObject) {
+          const dropdownItem = `<a class="dropdown-item text-center text-white" href="#">${stateName}</a>`;
+          ddMenuDiv.innerHTML += dropdownItem;
+        }
+      } else {
+        console.error("Error: Empty data or error fetching data");
+      }
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+// States Carousel
+
+  $('.plantCarousel').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      centerMode: false,
+      infinite: false,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 576,
+              settings: {
+                  slidesToShow: 1
+              }
+          }
+      ] 
+  });
 });
