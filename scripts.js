1c4b533649180bdb27adbb492ea54fda2a3dc31e
LeeWest89 <lee.west@atlasschool.com>
2024-04-10 22:35:19 -0500

    Updated the cards to work properly, need to update onestate.json to images: resultsImages/stateFlower/existing image data for it to communicate properly.

--- scripts.js
+++ scripts.js
@@ -23,28 +23,9 @@ $(document).ready(function() {
   setInterval(changeBackground, 10000);
 
 //DropDown
-
-  // const ddMenuDiv = document.getElementById("ddMenu");
-
-  // fetch("statesData.json")
-  //   .then(response => response.json())
-  //   .then(data => {
-
-  //   if (data && data.length > 0) {
-  //     ddMenuDiv.innerHTML = '';
-
-  //     data.forEach(stateObject => {
-  //       const stateName = stateObject.state;
-  //       const dropdownItem = `<a class="dropdown-item text-center text-white" href="#">${stateName}</a>`;
-  //       ddMenuDiv.innerHTML += dropdownItem;
-  //     });
-  //   } else {
-  //     console.error("Error: Empty data or error fetching data");
-  //   }
-  // })
-  // .catch(error => console.error('Error fetching data:', error));
   
   const ddMenuDiv = document.getElementById("ddMenu");
+  const plantCarouselDiv = document.getElementById("plantCarousel");
   
   fetch("onestate.json")
     .then(response => response.json())
@@ -58,15 +39,44 @@ $(document).ready(function() {
           const dropdownItem = `<a class="dropdown-item text-center text-white" href="#">${stateName}</a>`;
           ddMenuDiv.innerHTML += dropdownItem;
         }
+
+        for (const stateName in statesObject) {
+          const commonName = statesObject[stateName].commonName;
+          const scientificName = statesObject[stateName].scientificName;
+          const imageSrc = `images/${statesObject[stateName].image}`;
+
+          const plantCard = `
+            <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+              <div class="card">
+                <img src="${imageSrc}" class="card-img-top" alt="Video thumbnail"/>
+                <div class="card-body bg-white rounded-bottom">
+                  <p class="pb-2 pt-2 pl-2"><span class="bullet">&#8226;</span>${statesObject[stateName].stateFlower}</p>
+                  <p class="pb-2 pt-3 pl-2"><span class="bullet">&#8226;</span>${commonName}</p>
+                  <p class="pb-2 pt-2 pl-2"><span class="bullet">&#8226;</span>${scientificName}</p>
+                </div>
+              </div>
+            </div>
+          `;
+  
+          plantCarouselDiv.innerHTML += plantCard;
+          plantCarouselDiv.innerHTML += plantCard;
+          plantCarouselDiv.innerHTML += plantCard;
+          plantCarouselDiv.innerHTML += plantCard;
+          plantCarouselDiv.innerHTML += plantCard;
+        }
+  
+        startCarousel();
       } else {
         console.error("Error: Empty data or error fetching data");
       }
     })
     .catch(error => console.error('Error fetching data:', error));
 
-// States Carousel
+//Cards
 
-  $('.plantCarousel').slick({
+// States Carousel
+  function startCarousel() {
+    $('#plantCarousel').slick({
       slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: false,
@@ -74,18 +84,19 @@ $(document).ready(function() {
       centerMode: false,
       infinite: false,
       responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 2
-              }
-          },
-          {
-              breakpoint: 576,
-              settings: {
-                  slidesToShow: 1
-              }
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
           }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1
+          }
+        }
       ] 
-  });
+    });
+  }
 });
